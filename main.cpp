5c879313ecac39586c19ddd6ec8d8722497513b9
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2021-06-15 20:35:11 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,64 @@
+//
+// Created by Vasily Perekhrest on 15.06.2021.
+//
+
+#include <iostream>
+#include <vector>
+
+#include <flint/fmpzxx.h>
+#include <flint/fmpz_factorxx.h>
+
+
+int main() {
+    flint::fmpzxx base("2017");
+    flint::fmpzxx power("1"), module;
+
+    // module = module_base ^ module_exp
+    flint::fmpzxx module_base("10");
+    unsigned int module_exp = 100;
+
+    if (module_base == 10) {
+        fmpz_pow_ui(module._fmpz(), module_base._fmpz(), module_exp);
+
+        flint::fmpzxx d;
+        fmpz_gcd(d._fmpz(), base._fmpz(), module._fmpz());
+        if (d != 1) {
+            std::cout << base << " is not mutually simple with the module." << std::endl;
+            return -1;
+        }
+
+        for (int i = 0; i < module_exp; i++) {
+            fmpz_powm(power._fmpz(), base._fmpz(), power._fmpz(), module._fmpz());
+        }
+
+    } else {
+        fmpz_pow_ui(module._fmpz(), module_base._fmpz(), module_exp);
+
+        flint::fmpzxx d, phi(module);
+        std::vector<flint::fmpzxx> phi_list;
+
+        if (d != 1) {
+            std::cout << base << " is not mutually simple with the module." << std::endl;
+            return -1;
+        }
+
+        phi_list.push_back(phi);
+        for (int i = 0; i < module_exp; i++) {
+            fmpz_euler_phi(phi._fmpz(), phi._fmpz());
+            fmpz_gcd(d._fmpz(), phi._fmpz(), base._fmpz());
+            if (d != 1) {
+                std::cout << base << " is not mutually simple with the module." << std::endl;
+                return -1;
+            }
+            phi_list.push_back(phi);
+        }
+
+        for (int i = 0; i < module_exp + 1; i++) {
+            fmpz_powm(power._fmpz(), base._fmpz(), power._fmpz(), phi_list[module_exp - i]._fmpz());
+        }
+    }
+
+    std::cout << power << std::endl;
+
+    return 0;
+}
